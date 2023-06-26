fff88f4c3388fa25c1ae896b741039c9223a606c
Lucas Winkler <lucaswinkler@gmail.com>
2023-06-25 20:46:09 -0400

    Slightly modified the default image optimizer config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,62 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteImageOptimizer()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          'removeDimensions',
+          'removeStyleElement',
+          'removeScriptElement',
+          'removeComments',
+          {
+            name: 'cleanupIDs',
+            active: true,
+            params: {
+              minify: true,
+              remove: false,
+            },
+          },
+          'cleanupAttrs',
+          'removeMetadata',
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+        ],
+      },
+      png: {
+        quality: 75,
+      },
+      jpeg: {
+        quality: 75,
+        progressive: true,
+      },
+      jpg: {
+        quality: 75,
+        progressive: true,
+      },
+      tiff: {
+        quality: 75,
+      },
+      gif: {},
+      webp: {
+        quality: 75,
+        lossless: false,
+      },
+      avif: {
+        quality: 75,
+        lossless: false,
+      },
+    }),
+  ],
 });
