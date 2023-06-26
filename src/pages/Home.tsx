cbec305d9f74f6d1bbccc4c519bd829505e1535a
Lucas Winkler <lucaswinkler@gmail.com>
2023-06-26 16:47:40 -0400

    Added buttons to home to test on prod

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
+import ArrowRightIcon from '../assets/shared/desktop/icon-arrow-right.svg';
+
 export default function Home() {
   return (
     <>
       <div className='container'>
         <h1>Home Page</h1>
+        <div className='flex items-center gap-2'>
+          <button className='btn btn-primary'>Click me</button>
+          <button className='btn btn-secondary'>Click me</button>
+          <button className='btn btn-secondary-invert'>Click me</button>
+          <button className='btn btn-simple'>
+            Click me
+            <img src={ArrowRightIcon} alt='' />
+          </button>
+        </div>
       </div>
     </>
   );
