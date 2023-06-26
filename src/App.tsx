49f6469240e7ee4f84fe2fc9969daeea8a46fd37
Lucas Winkler <lucaswinkler@gmail.com>
2023-06-26 01:59:40 -0400

    Added most of the design system colours/font properties to the tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='font-bold'>Hello World!</h1>
+        <h1 className='text-orange'>Hello World!</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
           accusamus, deleniti corporis recusandae voluptatibus velit.
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,95 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      neutral: {
+        100: '#FFFFFF',
+        300: '#FAFAFA',
+        400: '#F1F1F1',
+        800: '#101010',
+        900: '#000000',
+      },
+      orange: {
+        light: '#FBAF85',
+        DEFAULT: '#D87D4A',
+      },
+    },
+    fontSize: {
+      xs: [
+        '0.8125rem', // 13px
+        {
+          lineHeight: '1.5625rem', // 25px
+          letterSpacing: '0.05806rem', // 0.93px
+          fontWeight: 700, // Bold
+        },
+      ],
+      sm: [
+        '0.875rem', // 14px
+        {
+          lineHeight: '1.1875rem', // 19px
+          letterSpacing: '0.625rem', // 10px
+          fontWeight: 400, // Regular
+        },
+      ],
+      base: [
+        '0.9375rem', // 15px
+        {
+          lineHeight: '1.5625rem', // 25px
+          letterSpacing: undefined,
+          fontWeight: 500, // Medium
+        },
+      ],
+      lg: [
+        '1.125rem', // 18px
+        {
+          lineHeight: '1.5rem', // 24px
+          letterSpacing: '0.08038rem', // 1.29px
+          fontWeight: 700, // Bold
+        },
+      ],
+      xl: [
+        '1.5rem', // 24px
+        {
+          lineHeight: '2.0625rem', // 33px
+          letterSpacing: '0.10713rem', // 1.71px
+          fontWeight: 700, // Bold
+        },
+      ],
+      '2xl': [
+        '1.75rem', // 28px
+        {
+          lineHeight: '2.375rem', // 38px
+          letterSpacing: '0.125rem', // 2px
+          fontWeight: 700, // Bold
+        },
+      ],
+      '3xl': [
+        '2rem', // 32px
+        {
+          lineHeight: '2.25rem', // 36px
+          letterSpacing: '0.07144rem', // 1.14px
+          fontWeight: 700, // Bold
+        },
+      ],
+      '4xl': [
+        '2.5rem', // 40px
+        {
+          lineHeight: '2.75rem', // 44px
+          letterSpacing: '0.08931rem', // 1.43px
+          fontWeight: 700, // Bold
+        },
+      ],
+      '5xl': [
+        '3.5rem', // 56px
+        {
+          lineHeight: '3.625rem', // 58px
+          letterSpacing: '0.125rem', // 2px
+          fontWeight: 700, // Bold
+        },
+      ],
+    },
     extend: {
       fontFamily: {
         sans: ['Manrope', ...defaultTheme.fontFamily.sans],
