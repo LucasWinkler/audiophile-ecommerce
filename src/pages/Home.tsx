2d1ffb33e6f41e8f2670a5065aa4892d0ae6a11b
Lucas Winkler <lucaswinkler@gmail.com>
2023-06-27 16:28:16 -0400

    Fixed incorrect styling for the black button.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className='flex items-center gap-2'>
           <button className='btn btn-primary'>Click me</button>
           <button className='btn btn-secondary'>Click me</button>
-          <button className='btn btn-secondary-invert'>Click me</button>
+          <button className='btn btn-secondary-alt'>Click me</button>
           <button className='btn btn-simple'>
             Click me
             <ArrowRightIcon />
