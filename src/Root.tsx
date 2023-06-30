a33700e75c30a831eff2f57da80e04e3aa2cb64c
Lucas Winkler <lucaswinkler@gmail.com>
2023-06-29 22:48:48 -0400

    Added placeholder footer component

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from './components/common/Navigation/Navigation';
+import Footer from './components/common/Footer/Footer';
 
 export default function Root() {
   return (
     <>
       <Navigation />
       <Outlet />
+      <Footer />
     </>
   );
 }
---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className='container'>
+        <h2>Footer</h2>
+      </div>
+    </footer>
+  );
+}
