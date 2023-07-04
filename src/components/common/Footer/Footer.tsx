5e3c25b8466ae59d7083c83f0a7ec825d418528f
Lucas Winkler <lucaswinkler@gmail.com>
2023-07-04 17:28:35 -0400

    Adjusted the spacing and breakpoints for the footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -8,13 +8,13 @@ import TwitterIcon from '../icons/TwitterIcon';
 
 export default function Footer() {
   return (
-    <footer className='relative bg-neutral-900 pb-[2.37rem] pt-[3.25rem] text-neutral-100 md:pb-[2.88rem] md:pt-[3.75rem] 2xl:pb-[3rem] 2xl:pt-[4.69rem]'>
+    <footer className='relative bg-neutral-900 pb-[2.37rem] pt-[3.25rem] text-neutral-100 sm:pb-[2.88rem] sm:pt-[3.75rem] 2xl:pb-[3rem] 2xl:pt-[4.69rem]'>
       <Container>
-        <span className='absolute left-1/2 top-0 block h-[0.25rem] w-[6.3125rem] -translate-x-1/2 bg-orange md:left-[unset] md:-translate-x-0'></span>
-        <div className='flex flex-col items-center md:items-stretch'>
-          <div className='flex flex-col items-center gap-12 md:items-start md:justify-between md:gap-8 lg:flex-row'>
+        <span className='absolute left-1/2 top-0 block h-[0.25rem] w-[6.3125rem] -translate-x-1/2 bg-orange sm:left-[unset] sm:-translate-x-0'></span>
+        <div className='flex flex-col items-center sm:items-stretch'>
+          <div className='flex flex-col items-center gap-12 sm:items-start sm:justify-between sm:gap-8 lg:flex-row'>
             <Logo />
-            <ul className='flex flex-col items-center gap-4 md:flex-row md:justify-start'>
+            <ul className='flex flex-col items-center gap-4 sm:flex-row sm:justify-start'>
               {navigationLinks.map(link => (
                 <li key={link.name}>
                   <DesktopNavigationLink to={link.href}>
@@ -24,13 +24,13 @@ export default function Footer() {
               ))}
             </ul>
           </div>
-          <p className='mt-12 text-center text-base opacity-50 md:mt-8 md:text-left lg:max-w-[33.75rem]'>
+          <p className='mt-12 text-center text-base opacity-50 sm:mt-8 sm:text-left lg:max-w-[33.75rem]'>
             Audiophile is an all in one stop to fulfill your audio needs. We're
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
             our demo facility - we're open 7 days a week.
           </p>
-          <div className='mt-12 flex flex-col justify-between gap-12 md:flex-row md:gap-0'>
+          <div className='mt-12 flex flex-col justify-between gap-12 sm:flex-row sm:gap-0 lg:mt-20 2xl:mt-12'>
             <p className='text-center text-base font-bold opacity-50'>
               Copyright 2021. All Rights Reserved
             </p>
