06bce8cc23ebaf47c59d23641c3497e0620e1895
Lucas Winkler <lucaswinkler@gmail.com>
2023-07-04 21:03:47 -0400

    Adjusted category spacing, image and text size

--- src/components/common/CategorySection/CategoryList/Category/Category.tsx
+++ src/components/common/CategorySection/CategoryList/Category/Category.tsx
@@ -15,15 +15,15 @@ export default function Category({
   onClick,
 }: CategoryProps) {
   return (
-    <li className='relative flex w-full flex-col items-center justify-center rounded-lg bg-neutral-400 p-4'>
+    <li className='relative flex w-full flex-col items-center justify-center rounded-lg bg-neutral-400 px-4 py-6 lg:py-7'>
       <div className='flex w-full flex-col items-center justify-center'>
         <img
-          className='absolute top-[-26%] h-[8rem] w-auto'
+          className='absolute top-[-26%] h-[9rem] w-auto xl:h-[10rem]'
           src={thumbnail}
           alt={`${name} Thumbnail`}
           loading='lazy'
         />
-        <p className='pt-[4.5rem] text-center text-base font-bold uppercase tracking-[0.06694rem]'>
+        <p className='pt-[4.5rem] text-center text-base font-bold uppercase tracking-[0.06694rem] text-neutral-900 xl:text-lg'>
           {name}
         </p>
         <Button
