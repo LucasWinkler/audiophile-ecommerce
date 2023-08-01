0acb9a2c91847ce747cac0debe41f1312c4dafef
Lucas Winkler <lucaswinkler@gmail.com>
2023-08-01 00:36:09 -0400

    Added dynamic title to product pages

--- src/pages/products/[categorySlug]/[productSlug].tsx
+++ src/pages/products/[categorySlug]/[productSlug].tsx
@@ -1,6 +1,5 @@
 import AudioGearSection from "@/components/common/AudioGearSection/AudioGearSection";
 import CategorySection from "@/components/common/CategorySection/CategorySection";
-import Container from "@/components/common/Container";
 import { Product } from "@/types";
 import { getProductDetailsBySlug, getProductList } from "@/utils/products";
 import Head from "next/head";
@@ -13,13 +12,10 @@ export default function Product({ product }: ProductProps) {
   return (
     <>
       <Head>
-        <title>{`Test | Audiophile e-commerce website`}</title>
+        <title>{`${product.name} | Audiophile e-commerce website`}</title>
       </Head>
 
       <main className="pb-[7.5rem] pt-[calc(2rem+var(--navigation-height))] md:pb-[6rem] md:pt-[calc(6.125rem+var(--navigation-height))] xl:pb-[10rem]">
-        <Container>
-          ID: {product.id} Name: {product.name}
-        </Container>
         <CategorySection className="mt-[7.5rem] md:mt-[7.5rem] xl:mt-[10rem]" />
         <AudioGearSection className="mt-[7.5rem] md:mt-[7.5rem] xl:mt-[10rem]" />
       </main>
