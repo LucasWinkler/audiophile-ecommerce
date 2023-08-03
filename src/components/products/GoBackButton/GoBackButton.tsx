9e770404cdecddbc36f273a69c6900e24cffe0a3
Lucas Winkler <lucaswinkler@gmail.com>
2023-08-02 21:32:43 -0400

    Added a button to return to previous page on the product details pages. Added placeholder elements for product details components

---
+++ src/components/products/GoBackButton/GoBackButton.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+type GoBackButtonProps = {
+  children: React.ReactNode;
+};
+
+export default function GoBackButton({ children }: GoBackButtonProps) {
+  const router = useRouter();
+
+  return (
+    <button
+      className="text-base text-neutral-900/50 underline-offset-4 hover:underline focus:underline"
+      onClick={() => {
+        router.back();
+      }}
+    >
+      {children}
+    </button>
+  );
+}
--- src/pages/products/[categorySlug]/[productSlug].tsx
+++ src/pages/products/[categorySlug]/[productSlug].tsx
@@ -1,5 +1,7 @@
 import AudioGearSection from "@/components/common/AudioGearSection/AudioGearSection";
 import CategorySection from "@/components/common/CategorySection/CategorySection";
+import Container from "@/components/common/Container";
+import GoBackButton from "@/components/products/GoBackButton/GoBackButton";
 import { Product } from "@/types";
 import { getProductBySlug, getProductList } from "@/utils/products";
 import Head from "next/head";
@@ -16,6 +18,44 @@ export default function Product({ product }: ProductProps) {
       </Head>
 
       <main className="pb-[7.5rem] pt-[calc(2rem+var(--navigation-height))] md:pb-[6rem] md:pt-[calc(6.125rem+var(--navigation-height))] xl:pb-[10rem]">
+        <Container>
+          <GoBackButton>Go Back</GoBackButton>
+        </Container>
+        {/* Product details */}
+        <section>
+          <Container>
+            <div className="flex gap-4">
+              <div>Image</div>
+              <div>Product</div>
+            </div>
+          </Container>
+        </section>
+        {/* Product features */}
+        <section>
+          <Container>
+            <div>
+              <div>Features</div>
+              <div>In the box</div>
+            </div>
+          </Container>
+        </section>
+        {/* Product showcase */}
+        <section>
+          <Container>
+            <div>Showcase</div>
+          </Container>
+        </section>
+        {/* You may also like */}
+        <section>
+          <Container>
+            <div>
+              <h2>You may also like</h2>
+              <div>
+                <div>Product</div>
+              </div>
+            </div>
+          </Container>
+        </section>
         <CategorySection className="mt-[7.5rem] md:mt-[7.5rem] xl:mt-[10rem]" />
         <AudioGearSection className="mt-[7.5rem] md:mt-[7.5rem] xl:mt-[10rem]" />
       </main>
